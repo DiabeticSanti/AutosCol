2a78339642049de7d30f4961b5b46081bee21c6a
julian.ramirez.wipay.aws <julian.ramirez@wipay.es>
2025-06-07 16:38:55 -0500

    Actualización del back y sus diferentes funciones

--- src/main/java/com/AutosCol/demo/models/Pago.java
+++ src/main/java/com/AutosCol/demo/models/Pago.java
@@ -9,7 +9,9 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
@@ -17,6 +19,8 @@ import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Pago {
 
     @Id
